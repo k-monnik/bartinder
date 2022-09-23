0df4f7e9b3e6cfcbcdd4c9face16acd7cbb6be4f
Kristen Monnik <nolaviolinist@gmail.com>
2022-09-22 22:08:11 -0400

    make adjustments to api to fix missing instructions

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ document.querySelector('button').addEventListener('click', getDrink)
 function getDrink() {
     let drink = document.querySelector('input').value
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drink}`)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
         .then((response) => response.json()) //parse response as JSON
         .then(data => {
             console.log(data.drinks[0])
             document.querySelector('h2').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('h3').innerText = data.drinks[0].strInstructions
+            document.querySelector('h4').innerText = data.drinks[0].strInstructions
         })
         .catch(err => {
             console.log(`error ${err}`)
